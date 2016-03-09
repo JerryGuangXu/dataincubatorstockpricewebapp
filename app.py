5b5a68628debf7ff54d91202df92a9c7e2928d6b
yutong pang <yutongpang@yutongs-iMac.home>
2016-03-09 15:42:51 -0500

    set up initial stockprice restful api

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 from flask import Flask, render_template, request, redirect
 from flask_restful import Resource, Api
-#This is a init commit from yutong
+import Quandl
+
+# This is a init commit from yutong
 app = Flask(__name__)
+
+# restful api
+api = Api(app)
+
+
+class StockPrice(Resource):
+    def put(self):
+        stock_sticker = "WIKI/" + request.form['stocksticker']
+        stockdata = Quandl.get(stock_sticker, returns="numpy", trim_start="2015-01-01", trim_end="2015-01-31")
+        return {"stockdata": list(stockdata.Close)}
+
+
+api.add_resource(StockPrice, '/stockprice')
+
 
 @app.route('/')
 def main():
-  return redirect('/index')
+    return redirect('/index')
+
 
 @app.route('/index')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
