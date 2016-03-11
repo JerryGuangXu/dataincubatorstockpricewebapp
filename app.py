1a3b1ad0ed05accc7a17745a4e2ce4cf7ee10ad1
yutong pang <yutongpang@yutongs-iMac.home>
2016-03-10 22:08:04 -0500

    finish prototype

--- app.py
+++ app.py
@@ -29,5 +29,24 @@ def index():
     return render_template('index.html')
 
 
+
+@app.route('/stockplot',methods=['POST'])
+def stockplot():
+    import numpy as np
+    stock_sticker = "WIKI/" + request.form['stocksticker']
+    checkeditems = request.form.getlist('check')
+    colors = ["A6CEE3", "#B2DF8A", "#33A02C", "#FB9A99"]
+    stockdata = Quandl.get(stock_sticker, returns="numpy", trim_start="2015-01-01", trim_end="2015-01-31")
+    from bokeh.plotting import figure, show, output_file, vplot
+    from bokeh.embed import components
+    plot = figure(x_axis_type = "datetime")
+    plot.title = "Data from Quandle WIKI set"
+    plot.xaxis.axis_label = 'Date'
+    plot.yaxis.axis_label = 'Price'
+    for i in range(len(checkeditems)):
+        plot.line(stockdata.Date,stockdata[checkeditems[i]], color=colors[i], legend=checkeditems[i])
+    script, div = components(plot)
+    return render_template('graph.html',script=script, div=div, text=stock_sticker)
+
 if __name__ == '__main__':
     app.run()
